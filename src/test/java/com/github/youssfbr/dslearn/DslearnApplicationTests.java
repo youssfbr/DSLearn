d27652b644f8a39a2670d60ba9180fd3062c4d3d
Alisson Youssf <youssfbr@gmail.com>
2024-01-08 19:17:45 -0300

    Backend project created

---
+++ src/test/java/com/github/youssfbr/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
