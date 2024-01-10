f156c99f0b6a438fa0f9c31f7a77afadb4c859b6
Alisson Youssf <youssfbr@gmail.com>
2024-01-09 21:05:27 -0300

    Lessons seed, fix: lessonsDone

--- src/main/java/com/github/youssfbr/dslearn/entities/Enrollment.java
+++ src/main/java/com/github/youssfbr/dslearn/entities/Enrollment.java
@@ -2,11 +2,10 @@ package com.github.youssfbr.dslearn.entities;
 
 import com.github.youssfbr.dslearn.entities.pk.EnrollmentPK;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -20,6 +19,9 @@ public class Enrollment {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private final Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {}
 
