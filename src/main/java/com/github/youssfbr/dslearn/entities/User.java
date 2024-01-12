b22dc1aeddcec0ec3c30f3232535c87dcf3eec21
Alisson Youssf <youssfbr@gmail.com>
2024-01-12 11:01:24 -0300

    Exceptions, validation, security

--- src/main/java/com/github/youssfbr/dslearn/entities/User.java
+++ src/main/java/com/github/youssfbr/dslearn/entities/User.java
@@ -1,12 +1,17 @@
 package com.github.youssfbr.dslearn.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -71,6 +76,39 @@ public class User implements Serializable {
 
     public List<Notification> getNotifications() {
         return notifications;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 
--- src/main/java/com/github/youssfbr/dslearn/repositories/EnrollmentRepository.java
+++ src/main/java/com/github/youssfbr/dslearn/repositories/EnrollmentRepository.java
@@ -1,7 +1,8 @@
 package com.github.youssfbr.dslearn.repositories;
 
+import com.github.youssfbr.dslearn.entities.Enrollment;
 import com.github.youssfbr.dslearn.entities.pk.EnrollmentPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnrollmentRepository extends JpaRepository<EnrollmentPK, Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
 }
