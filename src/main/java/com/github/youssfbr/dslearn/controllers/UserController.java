441e97a204d9e01d7c9bebfee20a8c812963ade0
Alisson Youssf <youssfbr@gmail.com>
2024-01-13 19:09:49 -0300

    User by id

---
+++ src/main/java/com/github/youssfbr/dslearn/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.github.youssfbr.dslearn.controllers;
+
+import com.github.youssfbr.dslearn.dtos.UserDTO;
+import com.github.youssfbr.dslearn.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+
+}
--- src/main/java/com/github/youssfbr/dslearn/services/UserService.java
+++ src/main/java/com/github/youssfbr/dslearn/services/UserService.java
@@ -1,13 +1,18 @@
 package com.github.youssfbr.dslearn.services;
 
+import com.github.youssfbr.dslearn.dtos.UserDTO;
 import com.github.youssfbr.dslearn.entities.User;
 import com.github.youssfbr.dslearn.repositories.UserRepository;
+import com.github.youssfbr.dslearn.services.exceptions.ResourceNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -17,6 +22,13 @@ public class UserService implements UserDetailsService {
 
     public UserService(UserRepository repository) {
         this.repository = repository;
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new UserDTO(entity);
     }
 
     @Override
